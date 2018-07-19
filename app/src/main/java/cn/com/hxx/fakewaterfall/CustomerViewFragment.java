ca5b806c316c9ae4fe95bd657616cb5fc3c024bd
54024867@qq.com <54024867@qq.com>
2018-07-19 17:06:11 +0800

    popwindow加入

--- app/src/main/java/cn/com/hxx/fakewaterfall/CustomerViewFragment.java
+++ app/src/main/java/cn/com/hxx/fakewaterfall/CustomerViewFragment.java
@@ -4,15 +4,24 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import cn.com.hxx.fakewaterfall.myView.ButtonCircleProgressBar;
+import cn.com.hxx.fakewaterfall.myView.layoutmanager.CustomerLayoutManager;
 import cn.com.hxx.fakewaterfall.uti.httputil.MyUtils;
 
 /**
@@ -28,6 +37,15 @@ public class CustomerViewFragment extends Fragment {
     private ButtonCircleProgressBar circle_bar;
     private Handler handler = new Handler();
     private int progress = 0;
+    private Button btn_pop1;
+    private Button btn_pop2;
+    private PopupWindow popupWindow1;
+    private PopupWindow popupWindow2;
+
+    private boolean isShowPop2 = false;
+    private List<String> stringList = new ArrayList<>();
+
+    private int pop1State;
 
     public static CustomerViewFragment getInstatnce(){
         CustomerViewFragment customerViewFragment = new CustomerViewFragment();
@@ -49,9 +67,35 @@ public class CustomerViewFragment extends Fragment {
         button1 = getActivity().findViewById(R.id.btn1);
         iv_image = getActivity().findViewById(R.id.iv_image);
         circle_bar = getActivity().findViewById(R.id.circle_bar);
+        circle_bar = getActivity().findViewById(R.id.circle_bar);
+        btn_pop1 = getActivity().findViewById(R.id.btn_pop1);
+        btn_pop2 = getActivity().findViewById(R.id.btn_pop2);
         circle_bar.setMax(100);
+
+        initPop();
+
         initView();
 
+    }
+
+    private void initPop() {
+        View popWin1 = LayoutInflater.from(getContext()).inflate(R.layout.pop_window_layout1, null);
+        View popWin2 = LayoutInflater.from(getContext()).inflate(R.layout.pop_window_layout2, null);
+        popupWindow1 = new PopupWindow(popWin1, 200, 200);
+//        popupWindow1.setBackgroundDrawable(null);
+//        popupWindow1.setOutsideTouchable(true);
+//        popupWindow1.setFocusable(true);    //若想点击外部也消失，此3方法必须加入
+
+        popupWindow2 = new PopupWindow(popWin2, ViewGroup.LayoutParams.MATCH_PARENT, 400);
+        popupWindow2.setAnimationStyle(R.style.anim_menu_bottombar);
+        RecyclerView recycle_view = popWin2.findViewById(R.id.recycle_view);
+        for (int i = 0 ; i < 60; i++){
+            String string = "我是" + i + "项";
+            stringList.add(string);
+        }
+        recycle_view.setLayoutManager(new LinearLayoutManager(getContext()));
+        MyAdapter myAdapter = new MyAdapter();
+        recycle_view.setAdapter(myAdapter);
     }
 
     private void initView() {
@@ -133,7 +177,46 @@ public class CustomerViewFragment extends Fragment {
             }
         });
 
+        btn_pop1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (popupWindow1 == null)return;
+                switch (pop1State % 6){
+                    case 0 :
+                        popupWindow1.showAsDropDown(btn_pop1, 0 ,0);
+                        break;
+                    case 1:
+                        popupWindow1.dismiss();
+                        break;
+                    case 2:
+                        popupWindow1.showAsDropDown(btn_pop1, 0, -btn_pop1.getHeight()*2);
+                        break;
+                    case 3:
+                        popupWindow1.dismiss();
+                        break;
+                    case 4:
+                        popupWindow1.showAtLocation(rl_child, Gravity.CENTER, 0 ,0);
+                        break;
+                    case 5:
+                        popupWindow1.dismiss();
+                        break;
+                }
+                pop1State++;
 
+            }
+        });
+        btn_pop2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (popupWindow2 == null)return;
+                if (isShowPop2){
+                    popupWindow2.dismiss();
+                }else {
+                    popupWindow2.showAtLocation(rl_parent,Gravity.BOTTOM, 0, 0);
+                }
+                isShowPop2 = !isShowPop2;
+            }
+        });
     }
 
     public Runnable runnable = new Runnable() {
@@ -149,4 +232,40 @@ public class CustomerViewFragment extends Fragment {
             handler.postDelayed(runnable, 50);
         }
     };
+
+
+    class MyAdapter extends RecyclerView.Adapter<MyViewHolder>{
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View inflate = LayoutInflater.from(getContext()).inflate(R.layout.customer_manager_item, parent, false);
+            MyViewHolder myViewHolder = new MyViewHolder(inflate);
+            return myViewHolder;
+        }
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+            String str = stringList.get(position);
+            holder.setContent(str);
+        }
+
+        @Override
+        public int getItemCount() {
+            return stringList.size();
+        }
+    }
+
+    static class MyViewHolder extends RecyclerView.ViewHolder{
+
+        private TextView tv_content;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            tv_content = itemView.findViewById(R.id.tv_content);
+        }
+
+        public void setContent(String str){
+            tv_content.setText(str);
+        }
+    }
 }
